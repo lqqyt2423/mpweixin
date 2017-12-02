74638ee379a39149149a98f0a5d8bdf3df093963
liqiang <974923609@qq.com>
2017-12-02 17:15:18 +0800

    remove frame

--- config/environments/production.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = {
-  saveImgDir: '/root/resources'
-};
--- models/connect.js
+++ models/connect.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const config = require('../config');
 
 mongoose.set('debug', true);
 
